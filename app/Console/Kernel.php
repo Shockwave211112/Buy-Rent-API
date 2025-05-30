3085c2bd62c8743e628487270250c30549390eaf
Ilya Kornienko <shockwave211112@yandex.ru>
2025-05-30 15:28:22 +0300

    Fix env examples, add cron docker

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -12,7 +12,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-         $schedule->command('orders:close-expired')->everyFiveMinutes();
+         $schedule->command('orders:close-expired')->everyMinute();
     }
 
     /**
