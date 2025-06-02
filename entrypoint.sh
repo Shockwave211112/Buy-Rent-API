b408e5afda6ab60b2316298284843bc80e89e154
Ilya <74099154+Shockwave211112@users.noreply.github.com>
2025-06-02 19:57:55 +0300

    Fix rights error on docker build

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 composer install
 
+chmod -R 775 /var/www/br/storage /var/www/br/bootstrap
+chown -R www-data:www-data /var/www/br/storage /var/www/br/bootstrap
+
 until nc -z db 3306; do
   echo "$(date) ⏳ Ожидаем запуска MySQL..."
   sleep 5
 done
 
 echo "✅ MySQL готов!"
-
 
 php artisan key:generate
 
